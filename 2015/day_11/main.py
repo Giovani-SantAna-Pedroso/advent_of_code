2e63d6a6f994fd1b523d5f6e74999f5a5487c9c0
Giovani Sant'Ana Ferreira Pedroso Vaz <giovanisfpv@gmail.com>
2025-08-07 17:50:52 -0300

    a

---
+++ 2015/day_11/main.py
@@ -0,0 +1,66 @@
+import sys
+
+# passowords = ["hijklmmn", 'abbceffg', 'abbcegjk']
+passowords = ["abcdefgh", 'ghijklmn']
+
+def firs_rule(password):
+    for i in range(len(password) -2):
+        if ord(password[i]) == (ord(password[i+1]) -1) == (ord(password[i+2])-2):
+            return True
+        ...
+
+    return False
+
+def sedond_rule(password):
+    if 'i' in password or 'l' in password or 'o' in password:
+        return False
+        ...
+
+    return True
+
+def thrid_rule(password):
+    # print("\n\n")
+    # print(password)
+    for i in range(len(password) -1):
+        if password[i] == password[i+1]:
+            # print("frist Pari", (password[i], password[i+1]))
+            # print(password[i+2:])
+            for j in  range( i+2, len(password) -1):
+                if password[j] == password[j+1]:
+                    # print("second Pari", (password[j], password[j+1]))
+                    return True
+
+
+    return False
+
+# print(ord("z"))
+def increase_password(string):
+    arr_str = list(string)
+    for i in range(len(arr_str) - 1, -1, -1):
+        ascii_char = ord(arr_str[i])
+        print(arr_str)
+        if ascii_char  <=121:
+
+            arr_str[i] = chr(ascii_char +1)
+            return "".join(arr_str)
+        else: 
+            arr_str[i] = 'a'
+
+        print(string[i], end="")
+
+    return "".join(arr_str)
+
+
+print(increase_password('xzz'))
+
+sys.exit(0)
+for i in passowords:
+    crr_pass = i
+
+    while True:
+        if firs_rule(crr_pass) and sedond_rule(crr_pass) and thrid_rule(crr_pass):
+            break
+        crr_pass = increase_password(crr_pass)
+
+
+    # print(i, "-> first:", firs_rule(i), "- second:", sedond_rule(i), "- thrid:", thrid_rule(i))
